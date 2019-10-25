73ac589993b36bd4db83d5a902ea3d89f252b807
Milan Vanalderwerelt <milan.vanalderwerelt@student.howest.be>
2019-10-25 21:25:39 +0200

    third commit

--- src/index.js
+++ src/index.js
@@ -2,91 +2,74 @@ import "./style.css";
 import data from './assets/data/coffees.json';
 {
 const showCoff = data => {
-    console.log(data.coffees);
+    
     const $list = document.querySelector(`.prices__list`);
-    console.log("jup");
+    
     data.coffees.forEach(coffee => {
-        console.log("en tot ier?");
+        
         if(coffee.plantbased === true){
-            console.log("yeeyee");
+            
             const $listitem = document.createElement('li');
             $listitem.classList.add(`price`);
-            $listitem.setAttribute('data', "id: '"+ coffee.id +"'");
+            $listitem.innerHTML = `
+                <a class="price__button" data-id="${coffee.id}">
+                    <span class="price__button__wrapper">
+                        <span class="price__button__name">${coffee.name}</span>
+                        <span class="price__button__amount">&euro; ${coffee.prices.medium}</span>
+                    </span>
+                    <span class="price__button__plus">+</span>
+                </a>`;
             $list.appendChild($listitem);
-
-            const $pricebutton = document.createElement('a');
-            $pricebutton.classList.add('price__button');
-
-            const $wrapper = document.createElement('span');
-            $wrapper.classList.add('price__button__wrapper');
-
-            const $name = document.createElement('span');
-            $name.classList.add('price__button__name');
-            $name.innerHTML = coffee.name;
-
-            const $amount = document.createElement('span');
-            $amount.classList.add('price__button__amount');
-            $amount.innerHTML = "&euro;" + coffee.prices.medium;
-
-            const $btn = document.createElement('button');
-            $btn.classList.add('price__button__plus');
-            $btn.innerHTML = `+`;
-            $btn.addEventListener("click", addOrder);
-
-            $listitem.appendChild($pricebutton);
-            $pricebutton.appendChild($wrapper);
-            $pricebutton.appendChild($btn);
-
-            $wrapper.appendChild($name);
-            $wrapper.appendChild($amount);
-
-        }
-    });
-};
-
-const addOrder = e => {
-    const $clickedItem =e.path[1].dataset.id;
-
-console.log($clickedItem);
-};
-
-
-const createOrder = () => {
-    const $orders = document.querySelector(`.orders`);
-    let $number;
-    data.coffees.forEach(coffee => {
-
-            const $order = document.createElement('li');
-            $order.classList.add('order');
-
-            const $title = document.createElement('span');
-            $title.classList.add('order__name');
-
-            const $amount = document.createElement('span');
-            $amount.classList.add('order__amount');
-            $amount.innerHTML = $number+"X"+{name};
-
-            const $price = document.createElement('span');
-            $price.classList.add('order__price');
-            $price.innerHTML = "&euro;" + {total};
-
-            const $btn = document.createElement('button');
-            $btn.classList.add('remove');
-            $btn.innerHTML = `x`;
-
-            $title.appendChild($amount);
-            $orders.appendChild($order);
-            $order.appendChild($price);
-            $order.appendChild($btn);
-
+            }
         });
     };
 
+const handleClick = e => {
+    console.log("1");
+
+    const $orders = document.querySelector(`.orders`);
+
+    const $wrapper = document.querySelector(`.orders__wrapper`);
+    $wrapper.classList.remove('hide');
+
+    const $empty = document.querySelector(`.emptystate`);
+    $empty.classList.add(`hide`); 
+
+    const clickedItem = e.path[1].dataset.id;
+
+    for(let i = 0; i< data.coffees.length;i ++){
+        console.log("1.1");
+        const coffee = data.coffees[i];
+        console.log(coffee);
+        console.log("2");
+        if(coffee.id == clickedItem) {
+            let $orderElements = [];
+            $orderElements.push(coffee.id);
+            console.log("3");
+            const $order = document.createElement('li');
+            $order.dataset.orderId = coffee.id;
+            $order.classList.add(`order`);
+            $order.innerHTML =
+                `<span class="order__name">
+                    <span class="order__amount">1X</span>${coffee.name}
+                </span>
+                <span class="order__price">&euro; ${coffee.prices.medium}</span>
+                <button class="remove">X</button>`;
+            $orders.appendChild($order);
+        }
+    }
+};
+
     const init = () => {
         showCoff(data);
+
+        const buttons = document.querySelectorAll(`.price__button__plus`);
+        buttons.forEach($button => $button.addEventListener(`click`, handleClick));
         console.log(data);
+
+        //const buttons = document.querySelectorAll(`.remove`);
+        //buttons.forEach($button => $button.addEventListener(`click`, handleClickRemove));
     };
-  
 
   init();
 
